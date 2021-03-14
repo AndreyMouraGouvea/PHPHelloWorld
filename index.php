26dcadaef151637e9a8c29b621440f962a74419f
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2021-03-13 23:05:36 -0300

    First step

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php
+
+    ini_set('default_charset', 'utf-8');
+    $nome =  "Andrey";
+    $idade = 15;
+    $email = "andreygouveia.05@gmail.com";
+    
+    echo '<h1>Olá <font color="#000099">'.$nome.'</font>, bem vindo! <br>
+    Você tem <font color="#65a5CD">'.$idade.'</font> anos, seu email é: "<font color="#000BFF">'.$email.'</font>"</h1>';
+    ?>
+    
+
+    
+</body>
+</html>
